97a4bb07368a9b378c188a4cd94c548bdf0a3a12
Filip Kuchta <filip.kuchta94@gmail.com>
2024-04-20 21:37:26 +0200

    Added some function prototypes.

--- app/main.c
+++ app/main.c
@@ -6,6 +6,8 @@
 int main()
 {
     printHello();
+    
+
     assert(true);
     printf("OK - all assertions passed.\n");
     return 0;
--- src/ip_manager.h
+++ src/ip_manager.h
@@ -1,5 +1,9 @@
+#ifndef E38D31D7_475B_4425_876D_CBA539831D99
+#define E38D31D7_475B_4425_876D_CBA539831D99
+
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 /**
  * @brief 
@@ -8,3 +12,32 @@
  * @return false 
  */
 bool printHello();
+
+/**
+ * @brief 
+ * 
+ * @param base 
+ * @param mask 
+ * @return int32_t 
+ */
+int32_t add(uint32_t base, char mask);
+
+/**
+ * @brief 
+ * 
+ * @param base 
+ * @param mask 
+ * @return int32_t 
+ */
+int32_t del(uint32_t base, char mask);
+
+/**
+ * @brief 
+ * 
+ * @param ip 
+ * @return char 
+ */
+char check(uint32_t ip);
+
+
+#endif /* E38D31D7_475B_4425_876D_CBA539831D99 */
