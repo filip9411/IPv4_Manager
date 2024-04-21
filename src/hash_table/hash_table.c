500fca0bf04450a550fe4d11fd472f8d18abdf2c
Filip Kuchta <filip.kuchta94@gmail.com>
2024-04-21 19:36:37 +0200

    Added some more features.

--- src/hash_table/hash_table.c
+++ src/hash_table/hash_table.c
@@ -1,5 +1,8 @@
 #include <stdint.h>
+#include <stdlib.h>
 #include "hash_table.h"
+#include <string.h>
+
 
 #define CAPACITY 50000 // Size of the HashTable.
 
@@ -9,7 +12,7 @@
  * @param str 
  * @return unsigned long 
  */
-uint32_t hash_function(char* str)
+uint32_t hashFunction(char* str)
 {
     uint32_t i = 0;
 
@@ -17,4 +20,30 @@ uint32_t hash_function(char* str)
         i += str[j];
 
     return i % CAPACITY;
+}
+
+typedef struct hash_table
+{
+    // Contains an array of pointers to items.
+    hash_table_item** items;
+    int size;
+    int count;
+} hash_table;
+
+/**
+ * @brief Create a item object
+ * 
+ * @param key 
+ * @param value 
+ * @return hash_table_item* 
+ */
+hash_table_item* createItem(uint32_t* key, char* value)
+{
+    // Creates a pointer to a new HashTable item.
+    hash_table_item* item = (hash_table_item*) malloc(sizeof(hash_table_item));
+    item->key = (uint32_t*) malloc(sizeof(uint32_t));
+    item->value = (char*) malloc(strlen(value) + 1);
+    memcpy(item->key, key, sizeof(uint32_t));
+    memcpy(item->value, value, sizeof(char));
+    return item;
 }
--- src/hash_table/hash_table.h
+++ src/hash_table/hash_table.h
@@ -1,5 +1,14 @@
 #ifndef D08DC2F2_EEE3_4E82_A17A_E058AFC647FB
 #define D08DC2F2_EEE3_4E82_A17A_E058AFC647FB
 
+// Defines the HashTable item.
+typedef struct hash_table_item
+{
+    uint32_t* key;
+    char* value;
+} hash_table_item;
+
+hash_table_item* createItem(uint32_t* key, char* value);
+
 
 #endif /* D08DC2F2_EEE3_4E82_A17A_E058AFC647FB */
