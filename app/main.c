c0f00809c0ad8c11ba825fd5a735ee97d6460126
Filip Kuchta <filip.kuchta94@gmail.com>
2024-04-22 15:08:04 +0200

    Added some test code.

--- app/main.c
+++ app/main.c
@@ -2,11 +2,22 @@
 #include <assert.h>
 
 #include "ip_manager.h"
+#include "hash_table.h"
+
+#define CAPACITY 500
 
 int main()
 {
     printHello();
     
+    hash_table *ht = createTable(CAPACITY);
+    insertItem(ht, 1U, 1U);
+    insertItem(ht, 2U, 2U);
+    insertItem(ht, 3U, 3U);
+    insertItem(ht, 4U, 4U);
+
+    printSearch(ht, 1U);
+    printSearch(ht, 2U);
 
     assert(true);
     printf("OK - all assertions passed.\n");
