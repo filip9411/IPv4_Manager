60345f556c03d8cf7651b0065bf0a67b4bb661e7
Filip Kuchta <filip.kuchta94@gmail.com>
2024-04-23 13:47:28 +0200

    Added some tests.

--- app/main.c
+++ app/main.c
@@ -7,8 +7,9 @@ int main()
 {   
     ip_container *root = initContainer();
 
-    add(root, 0x0A140000, 16);
-    add(root, 0x20408000, 20);
+    add(root, 0x0A140000, 16); // 10.20.0.0/16
+    add(root, 0x0A140000, 18); // 10.20.0.0/16
+    add(root, 0x20408000, 20); // 32.64.128.0/20
 
     printf("Check 10.20.0.0: %d\n", check(root, 0x0A140000));
     printf("Check 10.20.1.1: %d\n", check(root, 0x0A140101));
@@ -16,9 +17,14 @@ int main()
     printf("Check 32.64.144.0: %d\n", check(root, 0x20409000));
     printf("Check 192.168.1.1: %d\n", check(root, 0xC0A80101));
 
-    freeTrie(root);
+    del(root, 0x0A140000, 16);
+    del(root, 0x0A140000, 18);
+
+    printf("Check 10.20.0.0: %d\n", check(root, 0x0A140000));
+
 
     assert(true);
     printf("OK - all assertions passed.\n");
+    freeTrie(root);
     return 0;
 }
