4323f5d457c87e70e60458c92039dad63a7df4f9
Filip Kuchta <filip.kuchta94@gmail.com>
2024-04-23 11:02:54 +0200

    Added trie functions.

--- src/trie_data/trie_data.c
+++ src/trie_data/trie_data.c
@@ -1,1 +1,59 @@
+#include <stddef.h>
+#include <stdlib.h>
 #include "trie_data.h"
+
+#define MAX_MASK 32
+
+trie_node *createTrieNode() {
+    trie_node *node = (trie_node *)malloc(sizeof(trie_node));
+    if (node == NULL) {
+        return NULL;
+    }
+    node->mask = -1; // Ustawienie maski na -1 oznacza brak pasującego prefiksu
+    node->children[0] = NULL;
+    node->children[1] = NULL;
+    return node;
+}
+
+// Funkcja dodająca prefiks do drzewa trie
+void addPrefixToTrie(trie_node *root, uint32_t base, char mask) {
+    trie_node *current = root;
+
+    for (int i = MAX_MASK; i >= mask; i--) {
+        int bit = (base >> i) & 1;
+        if (current->children[bit] == NULL)
+            current->children[bit] = createTrieNode();
+        current = current->children[bit];
+    }
+    current->mask = mask;
+}
+
+// Funkcja sprawdzająca czy adres IP zawiera się w drzewie trie
+char checkInTrie(trie_node *root, uint32_t ip) {
+    trie_node *current = root;
+    char maxMask = -1;
+
+    for (int i = MAX_MASK; i >= 0; i--) {
+        int bit = (ip >> i) & 1;
+        if (current->children[bit] != NULL) {
+            current = current->children[bit];
+            if (current->mask != -1 && current->mask > maxMask)
+                maxMask = current->mask;
+        } else {
+            break;
+        }
+    }
+
+    return maxMask;
+}
+
+// Funkcja zwalniająca pamięć zajmowaną przez drzewo trie
+void freeTrie(trie_node *root) {
+    if (root == NULL)
+        return;
+
+    for (int i = 0; i < 2; i++)
+        freeTrie(root->children[i]);
+
+    free(root);
+}
--- src/trie_data/trie_data.h
+++ src/trie_data/trie_data.h
@@ -1,5 +1,35 @@
 #ifndef F2CE96C7_81D0_4F65_B556_2BB474BC8B66
 #define F2CE96C7_81D0_4F65_B556_2BB474BC8B66
 
+#include <stdint.h>
+typedef struct trie_node {
+    struct trie_node *children[2];
+    char mask;
+} trie_node;
+
+/**
+ * @brief 
+ * 
+ * @param root 
+ * @param base 
+ * @param mask 
+ */
+void addPrefixToTrie(trie_node *root, uint32_t base, char mask);
+
+/**
+ * @brief 
+ * 
+ * @param root 
+ * @param ip 
+ * @return char 
+ */
+char checkInTrie(trie_node *root, uint32_t ip);
+
+/**
+ * @brief 
+ * 
+ * @param root 
+ */
+void freeTrie(trie_node *root);
 
 #endif /* F2CE96C7_81D0_4F65_B556_2BB474BC8B66 */
