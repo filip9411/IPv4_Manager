e5a115fe1d81983cf08094b6ec2db19cb862311a
Filip Kuchta <filip.kuchta94@gmail.com>
2024-04-23 14:47:40 +0200

    Changes to tests.

--- app/main.c
+++ app/main.c
@@ -10,12 +10,11 @@ int main()
     assert(add(root, 1234, 45) == -1); // add() with wrong parameters
     assert(del(root, 1234, 45) == -1); // del() with wrong parameters
 
-    add(root, 0x20408000, 20); // 32.64.128.0/20
 
     add(root, 0x0A140000, 16); // 10.20.0.0/16
     assert(check(root, 0x0A140000) == 16U);
 
-    add(root, 0x0A142000, 24); // 10.20.0.0/24
+    add(root, 0x0A140000, 24); // 10.20.0.0/24
     assert(check(root, 0x0A140000) == 24U);
 
     assert(check(root, 0x0A141000) == 24U); // 10.20.1.0
@@ -25,6 +24,8 @@ int main()
 
     del(root, 0x0A140000, 16); // 10.20.0.0/16
     assert(check(root, 0x0A140000) == -1);
+
+    add(root, 0x20408000, 20); // 32.64.128.0/20
 
     printf("OK - all assertions passed.\n");
 
