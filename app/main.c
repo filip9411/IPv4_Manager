dc39bab518af0bb617fbddcfce9d5370bdc9fd61
Filip Kuchta <filip.kuchta94@gmail.com>
2024-04-23 16:28:48 +0200

    Cleaned tests.

--- app/main.c
+++ app/main.c
@@ -10,7 +10,6 @@ int main()
     assert(add(root, 1234, 45) == -1); // add() with wrong parameters
     assert(del(root, 1234, 45) == -1); // del() with wrong parameters
 
-
     add(root, 0x0A140000, 16); // 10.20.0.0/16
     assert(check(root, 0x0A140000) == 16U);
 
