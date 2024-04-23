90948ce2590d7e6dbd8aabfa537feceab0760094
Filip Kuchta <filip.kuchta94@gmail.com>
2024-04-23 12:51:35 +0200

    Added some function bodies.

--- src/ip_manager/ip_manager.c
+++ src/ip_manager/ip_manager.c
@@ -4,31 +4,35 @@
 #include"ip_manager.h"
 #include "trie_data.h"
 
+typedef trie_node ip_container;
+
 bool printHello()
 {
     printf("Hello World!!!\n");
 }
  
-trie_node* initContainer()
+ip_container* initContainer()
 {
     return createTrieNode();
 }
 
-int32_t add(trie_node* container, uint32_t base, char mask)
+int32_t add(ip_container* container, uint32_t base, char mask)
+{
+    if(mask <= 0U || mask > 32U) return -1;
+
+    addPrefixToTrie(container, base, mask);
+
+    return 0;
+}
+
+int32_t del(ip_container* container, uint32_t base, char mask)
 {
     if(mask <= 0U || mask > 32U) return -1;
 
     return 0;
 }
 
-int32_t del(trie_node* container, uint32_t base, char mask)
+char check(ip_container* container, uint32_t ip)
 {
-    if(mask <= 0U || mask > 32U) return -1;
-
-    return 0;
-}
-
-char check(trie_node* container, uint32_t ip)
-{
-    return 0;
+    return checkInTrie(container, ip);
 }
